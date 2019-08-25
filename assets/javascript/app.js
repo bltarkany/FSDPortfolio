751683cc1a749c57a5f93134e0e7ef35b357c63e
Bobbi Tarkany <bl.tarkany@gmail.com>
2019-08-25 00:17:58 -0700

    added all project info and links to github repositories and deployed sites. added scroll to top button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,34 @@
 //     var instances = M.Sidenav.init(elems, options);
 //   });
 
+
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 $(document).ready(function () {
 
     $('.sidenav').sidenav();
 
     $('.parallax').parallax();
+
+    window.onscroll = function () {
+        scrollFunction()
+    };
+    
+    $(document.body).on("click", "#myBtn", function () {
+        topFunction();
+    });
 });
